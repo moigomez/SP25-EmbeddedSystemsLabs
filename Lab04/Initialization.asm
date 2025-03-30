3276fe69d4ba31a7c89a9314bb71d757bbb65212
moigomez <moisesgomez04@icloud.com>
2025-03-29 19:56:09 -0500

    fix: update PWM code to match project specifications

--- Lab04/Initialization.asm
+++ Lab04/Initialization.asm
@@ -7,7 +7,7 @@
 ; ASM file to initialize PWM functionality for fan
 ;
 
-.include "m328pdef.inc" 
+.include "m328pdef.inc"
 
 .cseg
 .org 0x00
@@ -15,31 +15,27 @@ rjmp start
 
 .org 0x30
 start:
-    ; Set PB1 (OC1A) as output
-    sbi DDRB, PB1  
+    ; Set PD5 (OC0B) as output
+    sbi DDRD, PD5  
 
-    ; Configure Timer1 in Fast PWM mode (Mode 14)
-    ldi r16, (1 << WGM11)  ; WGM11: Fast PWM mode
-    sts TCCR1A, r16
+    ; Configure Timer0 for Fast PWM (Mode 7: WGM02:0 = 7)
+    ldi R16, (1 << WGM02)  ; Set WGM02
+    sts TCCR0A, R16
 
-    ldi r16, (1 << WGM13) | (1 << WGM12) | (1 << CS11)  ; Mode 14, Prescaler = 8
-    sts TCCR1B, r16
+    ldi R16, (1 << WGM01) | (1 << WGM00) | (1 << COM0B1)  ; Fast PWM, non-inverting
+    sts TCCR0A, R16
 
-    ; Set ICR1 for 25 kHz PWM frequency
-    ldi r16, LOW(79)
-    sts ICR1L, r16
-    ldi r16, HIGH(79)
-    sts ICR1H, r16
+    ; Set OCR0A for 25 kHz PWM frequency
+    ldi R16, 9
+    sts OCR0A, R16
 
-    ; Set initial duty cycle (50%)
-    ldi r16, LOW(40)
-    sts OCR1AL, r16
-    ldi r16, HIGH(40)
-    sts OCR1AH, r16
+    ; Set OCR0B for 50% duty cycle
+    ldi R16, 5
+    sts OCR0B, R16
 
-    ; Enable output compare mode (non-inverting mode)
-    ldi r16, (1 << COM1A1)
-    sts TCCR1A, r16
+    ; Set prescaler to 64 and start Timer0
+    ldi R16, (1 << CS01) | (1 << CS00)  ; Prescaler 64
+    sts TCCR0B, R16
 
 loop:
     rjmp loop  ; Infinite loop
