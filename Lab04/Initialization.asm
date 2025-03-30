a9c5ac7f44a3f6c690b748ff88be1291c8c6a894
moigomez <moisesgomez04@icloud.com>
2025-03-29 19:15:47 -0500

    feat: add direcotry and files for Lab04 project

---
+++ Lab04/Initialization.asm
@@ -0,0 +1,45 @@
+;
+; Initialization.asm
+;
+; Created: 3/27/2025
+; Author : Moises Gomez
+;
+; ASM file
+;
+
+.include "m328pdef.inc" 
+
+.cseg
+.org 0x00
+rjmp start
+
+.org 0x30
+start:
+    ; Set PB1 (OC1A) as output
+    sbi DDRB, PB1  
+
+    ; Configure Timer1 in Fast PWM mode (Mode 14)
+    ldi r16, (1 << WGM11)  ; WGM11: Fast PWM mode
+    sts TCCR1A, r16
+
+    ldi r16, (1 << WGM13) | (1 << WGM12) | (1 << CS11)  ; Mode 14, Prescaler = 8
+    sts TCCR1B, r16
+
+    ; Set ICR1 for 25 kHz PWM frequency
+    ldi r16, LOW(79)
+    sts ICR1L, r16
+    ldi r16, HIGH(79)
+    sts ICR1H, r16
+
+    ; Set initial duty cycle (50%)
+    ldi r16, LOW(40)
+    sts OCR1AL, r16
+    ldi r16, HIGH(40)
+    sts OCR1AH, r16
+
+    ; Enable output compare mode (non-inverting mode)
+    ldi r16, (1 << COM1A1)
+    sts TCCR1A, r16
+
+loop:
+    rjmp loop  ; Infinite loop
