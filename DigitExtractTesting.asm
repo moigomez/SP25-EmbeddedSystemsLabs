7a7ed72d6a6b2134dcf50e15ae0a367767ff347d
moigomez <moisesgomez04@icloud.com>
2025-04-09 12:50:12 -0500

    feat: replace code with ChatGPT div10, div100 subroutines

--- DigitExtractTesting.asm
+++ DigitExtractTesting.asm
@@ -8,36 +8,75 @@
 ; allows us to extract each digit of a multidigit number.
 ;
 
+; The below is ChatGPT code
 
-.include "m328Ppef.inc"
+.include "m328pdef.inc"
 
-.def temp = R16             ; Temporary register
-.def input_value = R24      ; Stores the chosen input
-.def result = R20           ; Stores the result of the function
+; Define duty cycle value for demonstration
+.duty_cycle_val: .byte 2   ; Two bytes (e.g. 0756 for 75.6%)
 
+; Program Start
 .org 0x0000
-  rjmp main
+rjmp main
 
-my_routine:
-  mov R18, R24
-  lsr R18
-  mov R25, R24
-  andi R25, lo8(-2)
-  add R25, R18
-  lsr R24
-  lsr R24
-  lsr R24
-  add R24, R25
-  swap R24
-  andi R24, lo8(15)
-  ret
-
+; --- Main Program ---
 main:
-  ldi input_value, 0b10110110   ; 182
+    ; Initialize Registers
+    ldi R16, 75      ; Set LSB of duty cycle (example: 75)
+    ldi R17, 6       ; Set MSB of duty cycle (example: 6)
+    sts duty_cycle_val, R16  ; Store LSB in memory
+    sts duty_cycle_val+1, R17 ; Store MSB in memory
 
-  rcall my_routine
-  ; The result of my_routine will be in R24 upon return.
-  mov result, R24
+    ; Call divide_by_100
+    lds R16, duty_cycle_val    ; Load LSB
+    lds R17, duty_cycle_val+1  ; Load MSB
+    rcall divide_by_100        ; Divides R17:R16 by 100, result in R18
 
-infinite_loop:
-  rjmp infinite_loop
+    ; Now R18 contains the hundreds place (quotient), R17:R16 contains the remainder
+
+    ; Call divide_by_10 with the remainder to get tens and ones digits
+    rcall divide_by_10        ; Divides the remainder (R17:R16) by 10
+    ; Now R18 contains tens place, R17 contains ones place
+
+    rjmp main  ; Loop forever (to simulate continuous display)
+
+; --- Subroutine to divide by 100 ---
+divide_by_100:
+    clr R18          ; Clear quotient (R18 = 0)
+div100_loop:
+    ldi R19, low(100)   ; Load low byte of 100
+    ldi R20, high(100)  ; Load high byte of 100
+    cp  R16, R19        ; Compare LSB
+    cpc R17, R20        ; Compare MSB
+    brlo div100_done    ; If R17:R16 < 100, we're done
+
+    ; Subtract 100 from R17:R16
+    subi R16, low(100)
+    sbci R17, high(100)
+
+    inc R18             ; Increment quotient (R18)
+    rjmp div100_loop    ; Repeat
+
+div100_done:
+    ret
+
+; --- Subroutine to divide by 10 ---
+divide_by_10:
+    clr R18          ; Clear quotient (R18 = 0)
+div10_loop:
+    ldi R19, 10      ; Load 10 to compare
+    clr R20
+    cp  R16, R19     ; Compare LSB with 10
+    cpc R17, R20     ; Compare MSB with 0
+    brlo div10_done  ; If less than 10, done
+
+    ; Subtract 10 from R17:R16
+    subi R16, 10
+    sbci R17, 0
+
+    inc R18          ; Increment quotient (R18)
+    rjmp div10_loop  ; Repeat
+
+div10_done:
+    mov R17, R16     ; Copy LSB to R17 (ones digit)
+    ret
