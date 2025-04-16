cd163987c6f2af27bb4caef7b164a8d06c2627c1
moigomez <moisesgomez04@icloud.com>
2025-04-15 19:34:49 -0500

    feat(LAB5): fix syntax issues, update ADC initialization, and add test code that outputs the voltage in ADC0 every 0.5s

--- Lab05/Lab5.c
+++ Lab05/Lab5.c
@@ -11,15 +11,16 @@
  */
 
 
-#define F_CPU 16000000UL    // Clock frequency (16MHz)
+#define F_CPU 16000000UL            // Clock frequency (16MHz)
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define FOSC 8000000UL      // Oscillator frequency (8MHz)
+#define FOSC 8000000UL              // Oscillator frequency (8MHz)
 #define BAUD 9600
-#define MYUBRR FOSC / 8 / BAUD - 1
-
-//#define INPUT_PIN PD5       // Define test pin
+#define MYUBRR FOSC / 8 / BAUD - 1  // Calculate UBRR0 register value
 
 void USART_Init(unsigned int ubrr) {
     // Set baud rate
@@ -32,9 +33,7 @@ void USART_Init(unsigned int ubrr) {
 }
 void USART_Transmit(unsigned char data) {
     // Wait for the transmit buffer to be empty
-    while (!(UCSR0A & (1 << UDRE0))) {
-        // Do nothing (wait)
-    }
+    while (!(UCSR0A & (1 << UDRE0)));
 
     // Put data into buffer, sends the data
     UDR0 = data;
@@ -49,39 +48,39 @@ void ADC_init() {
     // Set voltage reference
     ADMUX =  (1 << REFS1) | (1 << REFS0);
 
-
+    // Enable ADC and set prescaler of 128
+    ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 }
 uint16_t ADC_read(uint8_t channel) {
+    channel &= 0b00000111;
+    ADMUX = (ADMUX & 0xF8) | channel;
+
+    // Start single conversion
     ADCSRA |= (1 << ADSC);
 
+    // Wait for conversion to complete
     while (ADCSRA & (1 << ADSC));
 
-    return ADC
+    return ADC;
 }
 
 
 int main(void) {
     USART_Init(MYUBRR);
-
-    /*
-    // Set PD2 as input
-    DDRD &= ~(1 << INPUT_PIN);
-
-    // Optional: Enable pull-up resistor
-    PORTD |= (1 << INPUT_PIN);
-    */
+    ADC_init();
 
     while (1) {
-        /*
-        // Read the pin state
-        if (PIND & (1 << INPUT_PIN)) {
-            USART_SendString("Pin is HIGH\r\n");
-        } else {
-            USART_SendString("Pin is LOW\r\n");
-        }
-        */
+        char voltage_string[20];
+        char voltage_expression[20];
 
-        uint16_t adc_value = ADC_read(0);
+        uint16_t adc_value = ADC_read(0);   // Read ADC value
+        float voltage_value = (adc_value * 1.1) / 1024.0;   // Convert ADC value to voltage
+
+        dtostrf(voltage_value, 6, 3, voltage_string);   // Convert voltage float number to string
+        snprintf(voltage_expression, sizeof(voltage_expression), "v=%sV\r\n", voltage_string);  // Format voltage string
+
+        // Return voltage
+        USART_SendString(voltage_expression);
 
         _delay_ms(500);
     }
