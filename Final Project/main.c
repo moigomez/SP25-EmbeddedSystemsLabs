a51c52f16e9e41af76663b594001f1452424eb03
moigomez <moisesgomez04@icloud.com>
2025-04-26 15:47:36 -0500

    feat(FinProj): create folder and main file for final project code

---
+++ Final Project/main.c
@@ -0,0 +1,16 @@
+/*
+ *
+ * main.c
+ *
+ * Created: 4/26/2025
+ * Author:  Moises Gomez
+ *
+ * C-program for our Final Project that implements
+ * a 4x4 Tic-Tac-Toe game on an LCD with joysticks, 
+ * buzzers, score display, and a accelerometer/gyroscope
+ * sensor
+ * 
+ */
+
+
+ 
